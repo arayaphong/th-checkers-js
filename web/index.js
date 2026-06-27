3e3a11db158fbc5b624136628a9b210000392e8b
Arayaphong Traisopon <arayaphongt@wha-dogotal.com>
2026-06-27 14:46:11 +0700

    refactor: move index.js to the root directory and update script reference in index.html

---
+++ web/index.js
@@ -0,0 +1,9 @@
+import { Engine } from '../app/Engine.js';
+import { App } from './components/App/App.js';
+import { setTrace } from './utils/Debug.js';
+
+window.Engine = Engine;
+window.setTrace = setTrace;
+
+const app = new App();
+await app.mount();
